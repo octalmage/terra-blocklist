6b83c4de5371ffa18be557c44ca587066977ed16
Jason Stallings <jason@stallin.gs>
2022-05-20 07:19:12 -0500

    chore: Load  mnemonic from environment variable.

--- keys.terrain.js
+++ keys.terrain.js
@@ -2,11 +2,7 @@
 // to populate secret in CI environment instead of hardcoding
 
 module.exports = {
-  custom_tester_1: {
-    mnemonic:
-      "shiver position copy catalog upset verify cheap library enjoy extend second peasant basic kit polar business document shrug pass chuckle lottery blind ecology stand",
-  },
-  custom_tester_2: {
-    privateKey: "fGl1yNoUnnNUqTUXXhxH9vJU0htlz9lWwBt3fQw+ixw=",
+  bombay: {
+    mnemonic: process.env.SECRET_MNEMONIC,
   },
 };
